651bf3c97b66f0238ab7d7010efe623c11b59118
Takuya Okuda <92014661+okuda1506@users.noreply.github.com>
2021-11-23 23:06:35 +0900

    Update db.php

--- works/bulletinBoard/post/db.php
+++ works/bulletinBoard/post/db.php
@@ -1,7 +1,8 @@
 <?php
-$host = "";
-$dbName = "";
-$user = "";
-$password = "";
+//phpMyAdmin接続設定
+$host = ""; //個人情報保護のため空白
+$dbName = ""; //個人情報保護のため空白
+$user = ""; //個人情報保護のため空白
+$password = ""; //個人情報保護のため空白
 $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
 ?>
