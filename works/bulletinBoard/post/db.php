56817fafa2003948b2d7ca4a4979c92f87ccd7c1
Takuya Okuda <92014661+okuda1506@users.noreply.github.com>
2022-01-08 17:12:05 +0900

    Update db.php

--- works/bulletinBoard/post/db.php
+++ works/bulletinBoard/post/db.php
@@ -1,7 +1,7 @@
 <?php
-$host = "mysql57.takuyaokuda.sakura.ne.jp";
-$dbName = "takuyaokuda_portfolio_bbs";
-$user = "takuyaokuda";
-$password = "takuya56_portfolio";
+$host = "";
+$dbName = "";
+$user = "";
+$password = "";
 $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
 ?>
