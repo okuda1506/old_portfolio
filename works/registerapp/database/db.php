f5e03bb61001dbfec9a58aa54ef9d656c0d2a0b4
Takuya Okuda <92014661+okuda1506@users.noreply.github.com>
2022-02-08 16:14:49 +0900

    Update db.php

--- works/registerapp/database/db.php
+++ works/registerapp/database/db.php
@@ -1,6 +1,6 @@
 <?php
-$host = "mysql57.takuyaokuda.sakura.ne.jp";
-$dbName = "takuyaokuda_registerapp";
-$user = "takuyaokuda";
-$password = "takuya56_portfolio"; 
+$host = "";
+$dbName = "";
+$user = "";
+$password = ""; 
 $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
