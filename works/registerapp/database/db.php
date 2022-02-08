73565323231d47e687ea4bc1e3573c5585c6a2be
Takuya Okuda <92014661+okuda1506@users.noreply.github.com>
2022-02-08 16:13:59 +0900

    Add files via upload

---
+++ works/registerapp/database/db.php
@@ -0,0 +1,6 @@
+<?php
+$host = "mysql57.takuyaokuda.sakura.ne.jp";
+$dbName = "takuyaokuda_registerapp";
+$user = "takuyaokuda";
+$password = "takuya56_portfolio"; 
+$dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
